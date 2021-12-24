b61259a61ff21faa66472a6793204444565c5f72
c.sepulveda1 <olmac@Camilo.localdomain>
2021-12-23 23:03:09 -0500

    fix budgets

--- server.js
+++ server.js
@@ -12,5 +12,6 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(dirname+'/dist/ingreso/index.html'));
 });
 
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
